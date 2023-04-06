44cce79299e4430c3498547771d23b9aeb699158
alex <turdzeladze@gmail.com>
2023-04-06 13:27:28 +0400

    added basic authentication

--- shifts_manager/shifts_manager/urls.py
+++ shifts_manager/shifts_manager/urls.py
@@ -1,6 +1,9 @@
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
     path("", include("apimanager.urls")),
-    path("api/", include("apimanager.api.urls"))
+    path("admin", admin.site.urls),
+    path("api/", include("apimanager.api.urls")),
+    path("api-auth", include("rest_framework.urls"))
 ]
